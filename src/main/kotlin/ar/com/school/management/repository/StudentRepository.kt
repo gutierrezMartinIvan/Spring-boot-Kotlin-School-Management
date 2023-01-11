572de4795b70f6901995cf56d3c94a5b5f527b44
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-11 19:03:44 -0300

    added the repository annotation

--- src/main/kotlin/ar/com/school/management/repository/StudentRepository.kt
+++ src/main/kotlin/ar/com/school/management/repository/StudentRepository.kt
@@ -2,8 +2,10 @@ package ar.com.school.management.repository
 
 import ar.com.school.management.models.entity.StudentEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface StudentRepository : JpaRepository<StudentEntity, Long> {
     fun findBySocialSecurityNumber(socialSecurityNumber: Int): Optional<StudentEntity>
 }
