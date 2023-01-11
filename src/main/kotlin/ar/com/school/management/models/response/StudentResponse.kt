99e7ffc46f6087dbd6056c53da7eb3a26f444a59
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-11 01:02:02 -0300

    created the student request and response to use for the json requests and responses, still need to be done some more few changes

---
+++ src/main/kotlin/ar/com/school/management/models/response/StudentResponse.kt
@@ -0,0 +1,17 @@
+package ar.com.school.management.models.response
+
+import ar.com.school.management.models.entity.CareerEntity
+import ar.com.school.management.models.entity.SubjectEntity
+
+data class StudentResponse(var id: Long?,
+                           var socialSecurityNumber: Int?,
+                           var name: String?,
+                           var surname: String?,
+                           var phone: Int?,
+                           var email: String?,
+                           var password: String?,
+                           var careerId: CareerEntity?,
+                           var subjects: List<SubjectEntity>?,
+                           ) {
+    constructor(): this(null, null, null, null, null, null, null, null, null)
+}
