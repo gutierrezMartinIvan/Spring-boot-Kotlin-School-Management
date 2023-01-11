3746e30734b943c65ebb7fbe5a176e68ed84ff59
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-11 19:03:09 -0300

    added two new methods to use with career DTOs

--- src/main/kotlin/ar/com/school/management/utils/Mapper.kt
+++ src/main/kotlin/ar/com/school/management/utils/Mapper.kt
@@ -1,7 +1,10 @@
 package ar.com.school.management.utils
 
+import ar.com.school.management.models.entity.CareerEntity
 import ar.com.school.management.models.entity.StudentEntity
+import ar.com.school.management.models.request.CareerRequest
 import ar.com.school.management.models.request.StudentRequest
+import ar.com.school.management.models.response.CareerResponse
 import ar.com.school.management.models.response.StudentResponse
 import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,6 +17,9 @@ class Mapper {
     private lateinit var modelMapper: ModelMapper
 
     fun studentEntity2Dto(entity: StudentEntity): StudentResponse = modelMapper.map(entity, StudentResponse::class.java)
-
     fun studentDto2Entity(request: StudentRequest): StudentEntity = modelMapper.map(request, StudentEntity::class.java)
+    fun careerDto2Entity(request: CareerRequest): CareerEntity = modelMapper.map(request, CareerEntity::class.java)
+    fun careerEntity2Dto(entity: CareerEntity): CareerResponse = modelMapper.map(entity, CareerResponse::class.java)
+
+
 }
