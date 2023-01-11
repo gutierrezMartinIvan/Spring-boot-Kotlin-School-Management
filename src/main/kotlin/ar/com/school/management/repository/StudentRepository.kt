afe007c44c9e8da38f1783d646ff96fbefdcc6e7
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-11 00:59:11 -0300

    added the method to look up for a student by its social security number

--- src/main/kotlin/ar/com/school/management/repository/StudentRepository.kt
+++ src/main/kotlin/ar/com/school/management/repository/StudentRepository.kt
@@ -2,6 +2,8 @@ package ar.com.school.management.repository
 
 import ar.com.school.management.models.entity.StudentEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface StudentRepository: JpaRepository<StudentEntity, Long> {
+    fun findBySocialSecurityNumber(socialSecurityNumber: Int): Optional<StudentEntity>
 }
