1528088240fcec13f5d239b641e2450ace570a4c
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-11 12:31:03 -0300

    added the doc to each attribute of the error response class.

--- src/main/kotlin/ar/com/school/management/models/response/ApiErrorResponse.kt
+++ src/main/kotlin/ar/com/school/management/models/response/ApiErrorResponse.kt
@@ -1,7 +1,13 @@
 package ar.com.school.management.models.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.http.HttpStatus
 
-data class ApiErrorResponse(val status: HttpStatus,
+data class ApiErrorResponse(@Schema(description = "Http Status" , example = "Generic http status")
+                            val status: HttpStatus,
+
+                            @Schema(description = "Message error", example = "Generic message error")
                             val message: String?,
+
+                            @Schema(description = "List of errors", example = "Generic list of errors")
                             val errors: List<String>)
