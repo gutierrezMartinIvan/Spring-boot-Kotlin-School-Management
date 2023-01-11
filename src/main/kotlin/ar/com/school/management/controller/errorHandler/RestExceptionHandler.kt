cb9d597bcbbb75aa8da422da80c23dd543e80df9
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-11 15:46:33 -0300

    added the methods to validate attributes from a request and get back a custom error message

--- src/main/kotlin/ar/com/school/management/controller/errorHandler/RestExceptionHandler.kt
+++ src/main/kotlin/ar/com/school/management/controller/errorHandler/RestExceptionHandler.kt
@@ -4,11 +4,15 @@ import ar.com.school.management.exception.UserRegisteredException
 import ar.com.school.management.models.response.ApiErrorResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-
+@ControllerAdvice
 class RestExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(value = [UserRegisteredException::class])
@@ -19,5 +23,33 @@ class RestExceptionHandler : ResponseEntityExceptionHandler() {
             listOf("User Already Exists!")
         )
         return handleExceptionInternal(ex, error, HttpHeaders(), HttpStatus.CONFLICT, request)
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        var errors = mutableListOf<String>()
+        ex.bindingResult.fieldErrors.forEach { errors.add("${it.field}: ${it.defaultMessage}") }
+        ex.bindingResult.globalErrors.forEach { errors.add("${it.objectName}: ${it.defaultMessage}") }
+
+        val errorResponse = ApiErrorResponse(HttpStatus.BAD_REQUEST, "Please follow schema example", errors)
+        return handleExceptionInternal(
+            ex, errorResponse, headers, errorResponse.status, request
+        )
+    }
+
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val errorResponse = ApiErrorResponse(HttpStatus.BAD_REQUEST, ex.message, listOf(ex.httpInputMessage.toString(), ex.toString()))
+        return handleExceptionInternal(
+            ex, errorResponse, headers, errorResponse.status, request
+        )
     }
 }
