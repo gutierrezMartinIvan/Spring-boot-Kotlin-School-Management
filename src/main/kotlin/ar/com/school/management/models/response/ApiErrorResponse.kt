567070080faa30150b81dc91a1364ebeb0c4907c
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-14 01:03:00 -0300

    modified a little the annotations

--- src/main/kotlin/ar/com/school/management/models/response/ApiErrorResponse.kt
+++ src/main/kotlin/ar/com/school/management/models/response/ApiErrorResponse.kt
@@ -4,12 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.http.HttpStatus
 
 data class ApiErrorResponse(
-    @Schema(description = "Http Status", example = "Generic http status")
+    @field:Schema(description = "Http Status", example = "Generic http status")
     val status: HttpStatus,
 
-    @Schema(description = "Message error", example = "Generic message error")
+    @field:Schema(description = "Message error", example = "Generic message error")
     val message: String?,
 
-    @Schema(description = "List of errors", example = "Generic list of errors")
+    @field:Schema(description = "List of errors", example = "Generic list of errors")
     val errors: List<String>
 )
