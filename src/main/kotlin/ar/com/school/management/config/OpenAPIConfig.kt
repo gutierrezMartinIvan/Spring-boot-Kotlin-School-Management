fcff5dfac64820397f1babb950b6854eb8ee48a0
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-14 01:10:58 -0300

    Set up Swagger with the api info and contact.

---
+++ src/main/kotlin/ar/com/school/management/config/OpenAPIConfig.kt
@@ -0,0 +1,23 @@
+package ar.com.school.management.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenAPIConfig {
+    @Bean
+    fun customOpenAPI(): OpenAPI = OpenAPI().info(apiInfo())
+
+    private fun apiInfo(): Info = Info()
+        .title("University System Management and Functionalities")
+        .version("0.9")
+        .contact(apiContact())
+
+    private fun apiContact(): Contact = Contact()
+        .name("Martin Gutierrez")
+        .email("martin192012@gmail.com")
+        .url("https://www.linkedin.com/in/martgutierrez/")
+}
