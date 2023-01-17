8edb6aaf9b049d649473507f567e128a1c2523e1
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-17 20:13:02 -0300

    deleted unnecessary methods

--- src/main/kotlin/ar/com/school/management/utils/Mapper.kt
+++ src/main/kotlin/ar/com/school/management/utils/Mapper.kt
@@ -4,15 +4,12 @@ import ar.com.school.management.models.entity.CareerEntity
 import ar.com.school.management.models.entity.StudentEntity
 import ar.com.school.management.models.entity.SubjectEntity
 import ar.com.school.management.models.entity.TeacherEntity
-import ar.com.school.management.models.request.CareerRequest
-import ar.com.school.management.models.request.StudentRequest
-import ar.com.school.management.models.request.SubjectRequest
-import ar.com.school.management.models.request.TeacherRequest
+import ar.com.school.management.models.request.*
 import ar.com.school.management.models.response.*
+import org.modelmapper.Conditions
 import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.lang.RuntimeException
 
 @Component
 class Mapper {
@@ -20,18 +17,15 @@ class Mapper {
     @Autowired
     private lateinit var mm: ModelMapper
 
-    fun studentDto2Entity(request: StudentRequest): StudentEntity = mm.map(request, StudentEntity::class.java)
-    fun careerDto2Entity(request: CareerRequest): CareerEntity = mm.map(request, CareerEntity::class.java)
-    fun teacherDto2Entity(request: TeacherRequest): TeacherEntity = mm.map(request, TeacherEntity::class.java)
-    fun subjectDto2Entity(request: SubjectRequest): SubjectEntity = mm.map(request, SubjectEntity::class.java)
 
-    fun <S,D> entity2Dto(source: S, destinationType: Class<D>): D {
+
+    fun <S,D> map(source: S, destinationType: Class<D>): D {
         return when(val destination = mm.map(source, destinationType)) {
             is SubjectResponse -> fixNullSubjectResponse(destination)
             is TeacherResponse -> fixNullTeacherResponse(destination)
             is CareerResponse -> fixNullCareerResponse(destination)
             is StudentResponse -> fixNullStudentResponse(destination)
-            else -> throw RuntimeException("testing")
+            else -> return destination
         }
     }
 
