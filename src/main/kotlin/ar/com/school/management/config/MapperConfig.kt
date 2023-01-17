bfa8f3eab4610f87b14f849b9b720579ad78a19a
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-17 20:13:21 -0300

    clean up code

--- src/main/kotlin/ar/com/school/management/config/MapperConfig.kt
+++ src/main/kotlin/ar/com/school/management/config/MapperConfig.kt
@@ -1,5 +1,6 @@
 package ar.com.school.management.config
 
+import org.modelmapper.Conditions
 import org.modelmapper.ModelMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
