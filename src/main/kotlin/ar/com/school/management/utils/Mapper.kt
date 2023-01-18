96dc4da4b599e40afc0f470496c5b1d37b095bab
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-17 22:14:53 -0300

    clean up code

--- src/main/kotlin/ar/com/school/management/utils/Mapper.kt
+++ src/main/kotlin/ar/com/school/management/utils/Mapper.kt
@@ -1,12 +1,6 @@
 package ar.com.school.management.utils
 
-import ar.com.school.management.models.entity.CareerEntity
-import ar.com.school.management.models.entity.StudentEntity
-import ar.com.school.management.models.entity.SubjectEntity
-import ar.com.school.management.models.entity.TeacherEntity
-import ar.com.school.management.models.request.*
 import ar.com.school.management.models.response.*
-import org.modelmapper.Conditions
 import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -16,8 +10,6 @@ class Mapper {
 
     @Autowired
     private lateinit var mm: ModelMapper
-
-
 
     fun <S,D> map(source: S, destinationType: Class<D>): D {
         return when(val destination = mm.map(source, destinationType)) {
@@ -52,6 +44,4 @@ class Mapper {
         destination.careers = destination.careers ?: mutableListOf()
         return destination as D
     }
-
-
 }
