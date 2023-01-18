2fc2d61ac253df005e687cff09320b024182854a
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-17 23:36:09 -0300

    deleted unnecessary annotations

--- src/main/kotlin/ar/com/school/management/models/request/CareerRequest.kt
+++ src/main/kotlin/ar/com/school/management/models/request/CareerRequest.kt
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 
 data class CareerRequest(
-    @field:NotNull(message = "The name can't be null")
-    @field:NotEmpty(message = "the name can't be empty")
     @field:NotBlank(message = "the name can't be blank")
     @field:Schema(title = "The career's name", example = "Bachelor of Science in Computer and Information Systems")
     val name: String?,
--- src/main/kotlin/ar/com/school/management/models/request/SubjectRequest.kt
+++ src/main/kotlin/ar/com/school/management/models/request/SubjectRequest.kt
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 
 data class SubjectRequest(
-    @field:NotNull(message = "The name can't be null")
-    @field:NotEmpty(message = "the name can't be empty")
     @field:NotBlank(message = "the name can't be blank")
     @field:Schema(title = "The subject's name", example = "Programming 1")
     val name: String?
